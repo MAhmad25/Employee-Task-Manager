a55e5968ba7dba3ab3ad23dfeb66113c77d68f23
Muhammad Ahmad <ahmadlatifshadow@gmail.com>
2025-01-31 17:20:52 +0500

    Now Going For Vercel I hate GitHub

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Slide, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Context from "./Contexts/UserContext.jsx";
 createRoot(document.getElementById("root")).render(
       <Context>
-            <HashRouter>
+            <BrowserRouter>
                   <App />
                   <ToastContainer position="top-center" autoClose={1000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="colored" transition={Slide} />
-            </HashRouter>
+            </BrowserRouter>
       </Context>
 );
